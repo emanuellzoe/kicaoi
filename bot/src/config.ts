d0697ab12de7eb1a07d5cca47fd94b1d641e5be5
Axlnd3r <matthew.alexander@ti.ukdw.ac.id>
2026-06-15 23:53:30 +0700

    feat(bot): add fundPerWallet and funderPrivateKey to config

--- bot/src/config.ts
+++ bot/src/config.ts
@@ -57,4 +57,4 @@ export const WALLETS_FILE = fileURLToPath(new URL("../wallets.json", import.meta
 export const PUBLIC_FILE  = fileURLToPath(new URL("../wallets.public.json", import.meta.url));
 
 
-// [bot-ver] 1.1.1
+// [bot-ver] 1.2.0
